fc966ba3f9fdb2bcd7a4bcca5b62ecd1df680bfe
leviathan <jake66martin@hotmail.com>
2023-10-22 19:45:50 -0400

    trying to debug render on screen

--- routes/db.js
+++ routes/db.js
@@ -1,9 +1,46 @@
 const db = require('express').Router();
 const path = require('path');
+const fs = require('fs')
 
 
 db.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, '../db/db.json'))
 })
+
+db.post('/', (req, res) => {
+
+    const { title, text } = req.body;
+
+    if (title && text) {
+
+        const newNote = {
+            text,
+            title
+        }
+    
+        fs.readFile('./db/db.json', 'utf8', (err, data) => {
+            if (err) {
+              console.error(err);
+              return;
+            }
+      
+            const jsonData = JSON.parse(data)
+      
+          
+            jsonData.push(newNote)
+      
+      
+            fs.writeFile(`./db/db.json`, JSON.stringify(jsonData, null, 4), (err) =>
+            err
+              ? console.error(err)
+              : console.log(
+                  `Review for ${newNote.title} has been written to JSON file`
+                )
+          );
+          });
+    }
+
+    
+});
 
 module.exports = db
