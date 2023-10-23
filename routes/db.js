a97b85a09f31fdd2a72da9e7a3b3fcb4391191c7
leviathan <jake66martin@hotmail.com>
2023-10-22 20:42:52 -0400

    added uuid npm package

--- routes/db.js
+++ routes/db.js
@@ -1,11 +1,15 @@
 const db = require('express').Router();
 const path = require('path');
-const fs = require('fs')
+const fs = require('fs');
+const uuid = require('../helpers/uuid')
+
 
 
 db.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, '../db/db.json'))
-})
+});
+
+
 
 db.post('/', (req, res) => {
 
@@ -15,32 +19,41 @@ db.post('/', (req, res) => {
 
         const newNote = {
             text,
-            title
+            title,
+            note_id: uuid
         }
-    
+
         fs.readFile('./db/db.json', 'utf8', (err, data) => {
             if (err) {
-              console.error(err);
-              return;
+                console.error(err);
+                return;
             }
-      
+
             const jsonData = JSON.parse(data)
-      
-          
+
+
             jsonData.push(newNote)
-      
-      
+
+
             fs.writeFile(`./db/db.json`, JSON.stringify(jsonData, null, 4), (err) =>
-            err
-              ? console.error(err)
-              : console.log(
-                  `Review for ${newNote.title} has been written to JSON file`
-                )
-          );
-          });
+                err
+                    ? console.error(err)
+                    : console.log(
+                        `Note for ${newNote.title} has successfully been written to JSON file`
+                    )
+            );
+        });
     }
 
-    
+
 });
+
+
+
+db.delete('/', (req, res) => {
+
+});
+
+
 
 module.exports = db
